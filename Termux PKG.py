3e357fd76e3c673ec689b30969e2a31f032ad634
Donivan Doran <97486236+Donnyboy99@users.noreply.github.com>
2022-01-10 21:39:19 -0600

    Add files via upload

---
+++ Termux PKG.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+#Need to install git pkg install git
+#git clone https://github.com/Donnyboy99/Termux.git
+# install python pkg install python
+# install keyboard from pip python -m pip install keyboard
+
+
+
+
+x = str('pkg install nmap')
+print(x)     
+from keyboard import press
+press('enter')
+import time
+time.sleep(60)
+y = str('pkg install openssh')
+from keyboard import press
+press('enter')
+
